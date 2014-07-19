6ad1a77dcafaeb0eaa45d34f8de9813e767f27f9
Samy Pessé <samypesse@gmail.com>
2014-07-19 13:26:53 -0700

    Add default width for panel

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ define([
     var panel = new Panel();
 
     // Add panel to grid
-    codebox.app.grid.addView(panel);
+    codebox.app.grid.addView(panel, {
+        width: 20
+    });
 
     // Render the panel
     panel.render();
